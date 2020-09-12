d5ace7d99dfc90e78a1fa18eace5beae7943dff2
Швец Максим <shvetsmv@it2g.ru>
2020-09-12 17:41:50 +0300

    Updates

---
+++ precache-manifest.84794ec74f62a168f32ec571e847a938.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad8f6f4bee2f157737853e80cdadd63",
+    "url": "/lsd2020/index.html"
+  },
+  {
+    "revision": "329de5d8c8e3c59dd97c",
+    "url": "/lsd2020/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "2d99ba78375a87546968",
+    "url": "/lsd2020/static/css/main.e1926329.chunk.css"
+  },
+  {
+    "revision": "329de5d8c8e3c59dd97c",
+    "url": "/lsd2020/static/js/2.042b383f.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/lsd2020/static/js/2.042b383f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d99ba78375a87546968",
+    "url": "/lsd2020/static/js/main.a982c69c.chunk.js"
+  },
+  {
+    "revision": "5075d812de09ccd4ee2d",
+    "url": "/lsd2020/static/js/runtime-main.fab7af88.js"
+  },
+  {
+    "revision": "fdd88b742a9f6304f05b21b86ba58d9a",
+    "url": "/lsd2020/static/media/Roboto-Black.fdd88b74.woff"
+  },
+  {
+    "revision": "ac9a922232bb34219e80325fd9385afd",
+    "url": "/lsd2020/static/media/Roboto-BlackItalic.ac9a9222.woff"
+  },
+  {
+    "revision": "08cb8f79715774f9a6285ee7db2919a3",
+    "url": "/lsd2020/static/media/Roboto-Bold.08cb8f79.woff"
+  },
+  {
+    "revision": "65c7b0c96d644be47ca4a652b2d47972",
+    "url": "/lsd2020/static/media/Roboto-BoldItalic.65c7b0c9.woff"
+  },
+  {
+    "revision": "fd5970128586cba8e7ed994f134ec176",
+    "url": "/lsd2020/static/media/Roboto-Italic.fd597012.woff"
+  },
+  {
+    "revision": "10ad0f861c0c5807734017c341940649",
+    "url": "/lsd2020/static/media/Roboto-Light.10ad0f86.woff"
+  },
+  {
+    "revision": "1d77cf804e1c37bcb4bd0b7fc2a70208",
+    "url": "/lsd2020/static/media/Roboto-LightItalic.1d77cf80.woff"
+  },
+  {
+    "revision": "9a3bf7acae14d9b5ed5a88458106b58b",
+    "url": "/lsd2020/static/media/Roboto-Medium.9a3bf7ac.woff"
+  },
+  {
+    "revision": "cf9744aca2dc68046f03dacb46518dac",
+    "url": "/lsd2020/static/media/Roboto-MediumItalic.cf9744ac.woff"
+  },
+  {
+    "revision": "94dac78eee406a8c8f0406b69b85ac2b",
+    "url": "/lsd2020/static/media/Roboto-Regular.94dac78e.woff"
+  },
+  {
+    "revision": "f09d9b5e8e8a5f3843c1f0fa161fa23c",
+    "url": "/lsd2020/static/media/Roboto-Thin.f09d9b5e.woff"
+  },
+  {
+    "revision": "3ebe5873c57e5cd3bb164ae0585de453",
+    "url": "/lsd2020/static/media/Roboto-ThinItalic.3ebe5873.woff"
+  }
+]);
